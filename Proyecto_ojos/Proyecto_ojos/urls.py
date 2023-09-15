d75ee4fa700b23422826421be39f12e3bcb492ab
Manupr19 <124683456+Manupr19@users.noreply.github.com>
2023-09-15 18:21:23 +0200

    haciendo la interfaz

--- Proyecto_ojos/Proyecto_ojos/urls.py
+++ Proyecto_ojos/Proyecto_ojos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from interfaz.views import panel_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('panel/', panel_view, name='panel_view'),
 ]
--- Proyecto_ojos/interfaz/views.py
+++ Proyecto_ojos/interfaz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def panel_view(request):
+    letras = ["A", "B", "C", "D", "E", "F"]
+    return render(request, 'Proyecto_ojos\interfaz\panel.html', {'letras': letras})
